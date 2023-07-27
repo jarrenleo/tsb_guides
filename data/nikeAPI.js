b4b107035f195e899c3f58b8e7c657c0169aa0e3
Jarren Leo <jarren.leoo@gmail.com>
2023-07-27 23:05:58 +0800

    bug fix

--- data/nikeAPI.js
+++ data/nikeAPI.js
@@ -1,3 +1,5 @@
+import fetch from "node-fetch";
+
 export const getNikeGuideData = async (channel, sku, country, language) => {
   const response = await fetch(
     `https://api.nike.com/product_feed/threads/v3/?filter=marketplace(${country})&filter=language(${language})&filter=channelName(${channel})&filter=productInfo.merchProduct.styleColor(${sku})&filter=exclusiveAccess(true,false)`

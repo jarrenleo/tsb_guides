3b7375f6322b168307365fba880d210941b9e6b9
Jarren Leo <jarren.leoo@gmail.com>
2023-10-08 20:05:34 +0800

    commit

--- utilities/embeds.js
+++ utilities/embeds.js
@@ -33,7 +33,7 @@ export const guideEmbed = (
           inline: true,
         },
         {
-          name: "Region",
+          name: "Country",
           value: country,
           inline: true,
         },
